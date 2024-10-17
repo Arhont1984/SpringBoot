94081f4a6337e751c11a53ab5774623118324f5f
Kirill Granovsky <dj-kol@yandex.ru>
2024-10-17 19:03:53 +0300

    initial commit

---
+++ src/main/java/ru/netology/SpringBootDemo/SystemProfile/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.SpringBootDemo.SystemProfile;
+
+public interface SystemProfile {
+    String getProfile();
+}
---
+++ src/main/java/ru/netology/SpringBootDemo/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package ru.netology.SpringBootDemo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.netology.SpringBootDemo.SystemProfile.SystemProfile;
+
+@RestController
+@RequestMapping("/")
+public class ProfileController {
+    private SystemProfile profile;
+
+    public ProfileController(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
+    }
+
+
+}
